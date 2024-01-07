5c2eaee77f25e5659b762b373ce8ea3bc0db9446
ronilpatil <ronilpatil2000@gmail.com>
2024-01-07 12:51:03 +0530

    initial commit - DVC & git initilized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing data version control and model experimentation workflow using DVC & DVClive.',
+    author='ronilpatil',
+    license='MIT',
+)
